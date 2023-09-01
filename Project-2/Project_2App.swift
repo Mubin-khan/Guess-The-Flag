dc7f2eba90f1d2ad3b549aab501691012da79e77
Asadullah Pranto <asadullahpranto@gmail.com>
2023-09-01 11:06:10 +0600

    Initial Commit

---
+++ Project-2/Project_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_2App.swift
+//  Project-2
+//
+//  Created by Mubin Khan on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Project_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
